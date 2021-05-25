bbd466487aa6eb467a248c03e3e86641bd08ea65
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-05-25 19:35:13 +0300

    average function test

--- sum/sum.test.js
+++ sum/sum.test.js
@@ -1,45 +1,45 @@
-const fullname = require('./fullname');
+const sum = require('./sum');
 
-test('correct fullname', () => {
-    expect(fullname('Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
 });
 
-test('error without firstname', () => {
-    expect(fullname('', 'Pavardenis')).toBe(false);
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
 });
 
-test('error without lastname', () => {
-    expect(fullname('Vardenis', '')).toBe(false);
+test('adds 0.5 + 0.5 to equal 1', () => {
+    expect(sum(0.5, 0.5)).toBeCloseTo(1, 5);
 });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname(' Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
+test('adds 0.1 + 0.2 to equal 0.3', () => {
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 5);
 });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('Vardenis ', 'Pavardenis')).toBe('Vardenis Pavardenis');
+test('adds 13, 8 to equal 21', () => {
+    expect(sum(13, 8)).toBe(21);
 });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('     Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum('a', 8)).toBeFalsy();
 });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('Vardenis     ', 'Pavardenis')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum(NaN, 'a')).toBeFalsy();
 });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', ' Pavardenis')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum('a', NaN)).toBeFalsy();
 });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', 'Pavardenis ')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum(NaN, 8)).toBe(false);
 });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', '    Pavardenis')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum(8, NaN)).toBe(false);
 });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', 'Pavardenis     ')).toBe('Vardenis Pavardenis');
+test('string is not allowed', () => {
+    expect(sum(8, 'a')).toBeFalsy();
 });
